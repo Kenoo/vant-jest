a1916fc2491d5ea1d33956fe5e2b8f5fbc602e9f
czt <312008981@qq.com>
2021-10-24 19:04:59 +0800

    add deploy github page sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 发生任何错误时终止
+set -e
+
+# 构建
+yarn build
+
+# 进入输出产物文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:kenoo/vant-jest.git master:gh-pages
+cd -
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import styleImport from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/vant-jest/',
     plugins: [
         vue(),
         styleImport({
